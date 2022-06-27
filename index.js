2a2272ed8d85d8bf903618b0dc98c23299357443
Jan Erik Hanssen <jhanssen@gmail.com>
2022-06-26 20:52:59 -0700

    make it possible to add pregap

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 
 const input = process.argv[2];
 const output = process.argv[3];
+const pad = parseInt(process.argv[4]);
+if (isNaN(pad)) {
+    pad = 0;
+}
 
 if (!input || !output) {
     console.error("need an input and output file name");
@@ -17,9 +21,18 @@ try {
     process.exit(0);
 }
 
+let off = 0, num;
 const buf = Buffer.alloc(2352);
 
-let off = 0, num;
+// pad in number of sectors
+for (let i = 0; i < pad; ++i) {
+    num = fs.writeSync(outf, buf, 0);
+    if (num != 2352) {
+        console.error("wrote", num);
+        process.exit(1);
+    }
+}
+
 for (;;) {
     try {
         num = fs.readSync(inf, buf, 0, 2048, off);
@@ -29,6 +42,7 @@ for (;;) {
         num = fs.writeSync(outf, buf, 0);
         if (num != 2352) {
             console.error("wrote", num);
+            process.exit(1);
         }
 
         off += 2048;
