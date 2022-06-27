d7a606bb2e0503da9bfdea1034e0b3e53ffa8a7f
Jan Erik Hanssen <jhanssen@gmail.com>
2022-06-26 21:46:30 -0700

    the 16 byte header is part of the first sector

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 const input = process.argv[2];
 const output = process.argv[3];
-const pad = parseInt(process.argv[4]);
-const inSector = parseInt(process.argv[5]);
+let pad = parseInt(process.argv[4]);
+let inSector = parseInt(process.argv[5]);
 if (isNaN(pad)) {
     pad = 0;
 }
@@ -16,16 +16,19 @@ if (!input || !output) {
     process.exit(1);
 }
 
-let inf, outf;
+let inf, outf, sz;
 try {
     inf = fs.openSync(input, "r");
     outf = fs.openSync(output, "w");
+    const st = fs.fstatSync(inf);
+    sz = st.size;
 } catch (e) {
     console.error(e.message);
     process.exit(0);
 }
 
-let num;
+let num, written = 0;
+let stopAt = (sz / inSector) * 2352;
 const buf = Buffer.alloc(2352);
 
 // pad in number of sectors
@@ -44,6 +47,7 @@ if (inSector != 2352) {
         console.error("wrote", num);
         process.exit(1);
     }
+    stopAt -= 16;
 }
 
 for (;;) {
@@ -53,11 +57,16 @@ for (;;) {
             console.log("read", num);
             process.exit(0);
         }
-        num = fs.writeSync(outf, buf, 0);
+        if (written + 2352 > stopAt) {
+            num = fs.writeSync(outf, buf, 0, stopAt - written);
+        } else {
+            num = fs.writeSync(outf, buf, 0);
+        }
         if (num != 2352) {
             console.error("wrote", num);
             process.exit(1);
         }
+        written += num;
     } catch (e) {
         console.error(e.message);
         process.exit(0);
